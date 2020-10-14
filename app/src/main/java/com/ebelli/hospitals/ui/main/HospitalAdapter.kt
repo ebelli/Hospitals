233f52e7c399beda6e585ccf0f224ce780bb2b1a
enzobelli <ebelli@gmail.com>
2020-10-14 17:53:06 +0100

    Add Main Hospital List

---
+++ app/src/main/java/com/ebelli/hospitals/ui/main/HospitalAdapter.kt
@@ -0,0 +1,66 @@
+package com.ebelli.hospitals.ui.main
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.ebelli.hospitals.R
+import com.ebelli.hospitals.data.entities.HospitalEntity
+import kotlinx.android.synthetic.main.item_list_hospital.view.*
+
+const val HOSPITAL_EXTRA = "HOSPITAL"
+
+class HospitalAdapter: RecyclerView.Adapter<HospitalAdapter.HospitalViewHolder>() {
+
+
+    private lateinit var hospitals: List<HospitalEntity>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HospitalViewHolder {
+        val repoView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_hospital, parent, false) as LinearLayout
+
+        return HospitalViewHolder(repoView)
+    }
+
+    override fun onBindViewHolder(holder: HospitalViewHolder, position: Int) {
+        holder.bind(hospitals[position])
+    }
+
+    override fun getItemCount(): Int = hospitals.size
+
+    fun setData(hospitals: List<HospitalEntity>) {
+        this.hospitals = hospitals
+    }
+
+    class HospitalViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
+
+        fun bind(hospital: HospitalEntity) = with(view) {
+            hospital_name.text = hospital.organisationName
+            hospital_city.text = hospital.city
+
+//            itemView.setOnClickListener{ openDetails(hospital, itemView) }
+        }
+
+        private fun openAlbum(url: String, view: View) {
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, url)
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(view.context, shareIntent, null)
+        }
+
+//        private fun openDetails(album: HospitalEntity, view: View) {
+//            val intent = Intent(view.context, DetailActivity::class.java)
+//                .apply {
+//                    putExtra(HOSPITAL_EXTRA, album)
+//                }
+//            startActivity(view.context, intent, null)
+//        }
+    }
+}
--- app/src/main/java/com/ebelli/hospitals/ui/main/MainActivity.kt
+++ app/src/main/java/com/ebelli/hospitals/ui/main/MainActivity.kt
@@ -2,17 +2,67 @@ package com.ebelli.hospitals.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ebelli.hospitals.R
+import com.ebelli.hospitals.utils.Resource
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
     private val mainViewModel : MainViewModel by viewModel()
+    private val hospitalAdapter = HospitalAdapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRecyclerView()
+        setupObservers()
         mainViewModel.getHospitals()
+    }
+
+    private fun initRecyclerView() {
+        val recyclerView = hospital_list
+        val viewManager: RecyclerView.LayoutManager = LinearLayoutManager(this)
+
+        with(recyclerView) {
+            layoutManager = viewManager
+            adapter = hospitalAdapter
+        }
+    }
+
+    private fun setupObservers() {
+        mainViewModel.hospitals.observe(this, Observer {
+            it?.let { result ->
+                when (result) {
+                    is Resource.Loading -> {
+                        progressBar.visibility = View.VISIBLE
+                    }
+                    is Resource.Error -> {
+                        progressBar.visibility = View.GONE
+                        it.message?.let { message ->
+                            val snackbar = Snackbar.make(
+                                root,
+                                message, Snackbar.LENGTH_LONG
+                            )
+                            snackbar.show()
+                        }
+                    }
+                    is Resource.Success -> {
+                        it.data?.let {hospitals ->
+                            hospitalAdapter.setData(hospitals)
+                            hospitalAdapter.notifyDataSetChanged()
+                            hospital_list.visibility = View.VISIBLE
+                        }
+                        progressBar.visibility = View.GONE
+                    }
+                }
+            }
+        })
     }
 }
